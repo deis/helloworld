1a83bc8ebe2fbef8c38d9c302b74793b53e230ce
Gabriel Monroy <gabriel@opdemand.com>
2014-05-12 17:15:18 -0600

    fix(port): allow PORT binding or default to 80

--- helloworld.go
+++ helloworld.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
+	http.HandleFunc("/", hello)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(w, "Welcome to Deis!")
-    fmt.Fprintln(w, "See the documentation at http://docs.deis.io/ for more information.")
+	fmt.Fprintln(w, "Welcome to Deis!")
+	fmt.Fprintln(w, "See the documentation at http://docs.deis.io/ for more information.")
 }
